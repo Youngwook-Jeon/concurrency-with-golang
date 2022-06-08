8e4158f470cdb148cb74ac2ca014d21d0ec544f6
Youngwook-Jeon <ela87@naver.com>
2022-06-08 14:18:21 +0900

    Add our consumer

--- producer-consumer/main.go
+++ producer-consumer/main.go
@@ -87,7 +87,7 @@ func pizzeria(pizzaMaker *Producer) {
 			case pizzaMaker.data <- *currentPizza:
 
 			case quitChan := <-pizzaMaker.quit:
-				// close channel
+				// close channels
 				close(pizzaMaker.data)
 				close(quitChan)
 				return
@@ -114,6 +114,39 @@ func main() {
 	go pizzeria(pizzaJob)
 
 	// create and run consumer
+	for i := range pizzaJob.data {
+		if i.pizzaNumber <= NumberOfPizzas {
+			if i.success {
+				color.Green(i.message)
+				color.Green("Order #%d is out for delivery!", i.pizzaNumber)
+			} else {
+				color.Red(i.message)
+				color.Red("The customer is really mad!")
+			}
+		} else {
+			color.Cyan("Done making pizzas...")
+			err := pizzaJob.Close()
+			if err != nil {
+				color.Red("*** Error closing channel!", err)
+			}
+		}
+	}
 
 	// print out the ending message
+	color.Cyan("-----------------")
+	color.Cyan("Done for the day.")
+	color.Cyan("We made %d pizzas, but failed to make %d, with %d attempts in total.", pizzasMade, pizzasFailed, total)
+
+	switch {
+	case pizzasFailed > 9:
+		color.Red("It was an awful day...")
+	case pizzasFailed >= 6:
+		color.Red("It was not a very good day...")
+	case pizzasFailed >= 4:
+		color.Yellow("It was an okay day...")
+	case pizzasFailed >= 2:
+		color.Yellow("It was a pretty good day!")
+	default:
+		color.Green("It was a great day!")
+	}
 }
